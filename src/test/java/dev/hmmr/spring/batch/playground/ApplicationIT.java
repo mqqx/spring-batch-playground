70a229400f455e14537685a25b3263587106a5cc
mqqx <github@mqqx.dev>
2023-02-11 13:15:36 +0700

    ✨ Add Feature * Add initial base of `spring-batch-playground` to start developing from

---
+++ src/test/java/dev/hmmr/spring/batch/playground/ApplicationIT.java
@@ -0,0 +1,11 @@
+package dev.hmmr.spring.batch.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationIT {
+
+  @Test
+  void contextLoads() {}
+}
